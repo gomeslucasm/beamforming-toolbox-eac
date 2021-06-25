ed96a6a51e8b37810edaa165789c14ab38360d0d
Lucas Gomes <lmgomes96@gmail.com>
2021-06-25 12:00:28 -0300

    add opção para passar diferentes dimensões da imagem para gerar os pontos focais

--- Functions/FocusPoints.m
+++ Functions/FocusPoints.m
@@ -5,8 +5,14 @@ function Output = FocusPoints(Size,FrameSize,varargin)
     for i=1:2:length(varargin)
         sArgs(varargin{i}) = varargin{i+1};
     end
-    x_grid = -(Size/2):FrameSize:(Size/2);
-    y_grid = 0:FrameSize:Size;
+    if isscalar(Size)
+        x_grid = -(Size/2):FrameSize:(Size/2);
+        y_grid = 0:FrameSize:Size;
+    else
+        x_grid = Size(1,1):FrameSize:Size(1,2);
+        y_grid = Size(2,1):FrameSize:Size(2,2);
+    end
+    
     [X,Y] = meshgrid(x_grid,y_grid);   
     X = X(:);
     Y = Y(:);
